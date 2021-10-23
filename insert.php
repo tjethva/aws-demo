45e881a18ebce525c1ca2050dffdedcb600c9b01
Heena Jethva <tjethva1812@gmail.com>
2021-10-23 16:16:41 +0530

    My first AWS deploy

---
+++ insert.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Insert Page page</title>
+</head>
+
+<body>
+	<center>
+		<?php
+
+		$conn = mysqli_connect("myfirstdatabase.ci257tbv78se.us-west-1.rds.amazonaws.com", "admin", "hj232340", "myfirstdatabase");
+		
+		// Check connection
+		if($conn === false){
+			die("ERROR: Could not connect. "
+				. mysqli_connect_error());
+		}
+		
+		// Taking all 5 values from the form data(input)
+		$empId = 1;
+		$first_name = $_REQUEST['first_name'];
+		$last_name = $_REQUEST['last_name'];
+		$email = $_REQUEST['email'];
+		
+		// Performing insert query execution
+		// here our table name is college
+		$sql = "INSERT INTO college VALUES ('$first_name',
+			'$last_name','$gender','$address','$email')";
+		
+		if(mysqli_query($conn, $sql)){
+			echo "<h3>data stored in a database successfully."
+				. " Please browse your localhost php my admin"
+				. " to view the updated data</h3>";
+
+			echo nl2br("\n$first_name\n $last_name\n "
+				. "\n $email");
+		} else{
+			echo "ERROR: Hush! Sorry $sql. "
+				. mysqli_error($conn);
+		}
+		
+		// Close connection
+		mysqli_close($conn);
+		?>
+	</center>
+</body>
+
+</html>
+
